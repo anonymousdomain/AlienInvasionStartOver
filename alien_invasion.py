43d1399087ce4d3c53ab74f381434a5d660592d5
domainanonymous1 <domainanonymous1@gmail.com>
2020-08-21 18:43:05 +0300

    the game goes the full screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,8 +9,11 @@ class AlienInvasion:
         pygame.init()
         self.settings = Setting()
         # initalize the screen
-        self.screen = pygame.display.set_mode((self.settings.screen_width,
-                                               self.settings.screen_height))
+        #self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               #self.settings.screen_height))
+        self.screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_width=self.screen.get_rect().width 
+        self.settings.screen_height=self.screen.get_rect().height
         self.ship = Ship(self)
         # set a caption
         pygame.display.set_caption("dev Godof War")
