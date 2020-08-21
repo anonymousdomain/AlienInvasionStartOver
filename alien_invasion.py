76eb25628b723e79eac038850e43f81f04ce1d1a
domainanonymous1 <domainanonymous1@gmail.com>
2020-08-21 17:06:02 +0300

    a setting module add to organize things

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,20 +1,22 @@
 import pygame
 import sys
-
+from settings import Setting
 class AlienInvasion:
     def __init__(self):
         pygame.init()
+        self.settings=Setting()
         #initalize the screen
-        self.screen=pygame.display.set_mode((1200,600))
+        self.screen=pygame.display.set_mode((self.settings.screen_width,
+        self.settings.screen_height))
         #set a caption
         pygame.display.set_caption("dev Godof War")
-        self.bg_color=(255,255,255)
+        
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 if __name__ == "__main__":
     ai=AlienInvasion()
