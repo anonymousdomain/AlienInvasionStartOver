a446e4f3bfa51dc646810822df8344df8e7c31b2
domainanonymous1 <domainanonymous1@gmail.com>
2020-08-21 17:28:17 +0300

    control the ship move right & left

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,25 +2,34 @@ import pygame
 import sys
 from settings import Setting
 from ship import Ship
+
+
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.settings=Setting()
-        #initalize the screen
-        self.screen=pygame.display.set_mode((self.settings.screen_width,
-        self.settings.screen_height))
-        self.ship=Ship(self)
-        #set a caption
+        self.settings = Setting()
+        # initalize the screen
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               self.settings.screen_height))
+        self.ship = Ship(self)
+        # set a caption
         pygame.display.set_caption("dev Godof War")
-        
+
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key==pygame.K_RIGHT:
+                        self.ship.rect.x+=1
+                    if event.key == pygame.K_LEFT:
+                        self.ship.rect.x-=1
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
             pygame.display.flip()
+
+
 if __name__ == "__main__":
-    ai=AlienInvasion()
+    ai = AlienInvasion()
     ai.run_game()
--- ship.py
+++ ship.py
@@ -1,14 +1,16 @@
 import pygame
 
+
 class Ship:
-    def __init__(self,devo_game):
-        # the argument devo_game used to call the instance of alineInvasion 
-        self.screen=devo_game.screen
-        self.screen_rect=self.screen.get_rect()
-        #load the image ship
-        self.image=pygame.image.load("image/spaceship.bmp")
-        self.rect=self.image.get_rect()
-        #postion the ship in midd bottom
-        self.rect.midbottom=self.screen_rect.midbottom
+    def __init__(self, devo_game):
+        # the argument devo_game used to call the instance of alineInvasion
+        self.screen = devo_game.screen
+        self.screen_rect = self.screen.get_rect()
+        # load the image ship
+        self.image = pygame.image.load("image/spaceship.bmp")
+        self.rect = self.image.get_rect()
+        # postion the ship in midd bottom
+        self.rect.midbottom = self.screen_rect.midbottom
+
     def blitme(self):
-        self.screen.blit(self.image,self.rect)
+        self.screen.blit(self.image, self.rect)
