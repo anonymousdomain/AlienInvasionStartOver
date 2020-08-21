63404bc3331c1982dc358df6a0486c1e0c463462
domainanonymous1 <domainanonymous1@gmail.com>
2020-08-21 17:20:52 +0300

    call the ship class in the while loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from settings import Setting
+from ship import Ship
 class AlienInvasion:
     def __init__(self):
         pygame.init()
@@ -8,6 +9,7 @@ class AlienInvasion:
         #initalize the screen
         self.screen=pygame.display.set_mode((self.settings.screen_width,
         self.settings.screen_height))
+        self.ship=Ship(self)
         #set a caption
         pygame.display.set_caption("dev Godof War")
         
@@ -17,6 +19,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 if __name__ == "__main__":
     ai=AlienInvasion()
